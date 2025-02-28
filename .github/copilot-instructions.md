5cd26c91c885623a46f432b9fb9aba914b59e9de
Ferhat Konas <fkonas@gmail.com>
2025-02-28 13:52:12 +0300

    V01.02 Crash Course for Beginners
    Made a basic Tank prototype in this lecture.

---
+++ .github/copilot-instructions.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: Top-Down-Shooter-Game
+- Unity version: Unity 2022.3.52f1
+- Active game object:
+  - Name: Tank
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,109 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody rb;
+
+    [Header("Gun Data")]
+    [SerializeField] private Transform gunPoint;
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private GameObject bulletPrefab;
+
+    [Header("Movement Data")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed;
+
+    [Header("Input Data")]
+    private float verticalInput;
+    private float horizontalInput;
+
+    [Header("Tower Data")]
+    [SerializeField] private Transform towerTransform;
+    [SerializeField] private float towerRotationSpeed;
+
+    [Header("Aim Data")]
+    [SerializeField] private Transform aimTransform;
+    [SerializeField] private LayerMask whatIsAimMask;
+
+
+    //***************************//
+
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        UpdateAim();
+
+        CheckInputs();
     }
+
+    private void CheckInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (verticalInput < 0)
+        {
+            horizontalInput = -Input.GetAxis("Horizontal");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        ApllyMovement();
+        ApplyBodyRotation();
+        ApplyTowersRotation();
+    }
+
+    private void Shoot()
+    {
+        Debug.Log("Phew !");
+        GameObject bullet = Instantiate(bulletPrefab, gunPoint.position, gunPoint.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = gunPoint.forward * bulletSpeed;
+
+        Destroy(bullet, 3f);
+    }
+
+    private void ApplyTowersRotation()
+    {
+        Vector3 direction = aimTransform.position - towerTransform.position;
+        direction.y = 0;
+
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        towerTransform.rotation = Quaternion.RotateTowards(towerTransform.rotation, targetRotation, towerRotationSpeed);
+    }
+
+    private void ApplyBodyRotation()
+    {
+        transform.Rotate(0, horizontalInput * rotationSpeed, 0);
+    }
+
+    private void ApllyMovement()
+    {
+        Vector3 movement = transform.forward * moveSpeed * verticalInput;
+        rb.velocity = movement;
+    }
+
+    private void UpdateAim()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, whatIsAimMask))
+        {
+            float fixedY = aimTransform.position.y;
+            aimTransform.position = new Vector3(hit.point.x, fixedY, hit.point.z);
+        }
+    }
+
+
 }
